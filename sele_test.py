5db0df1803ad42293e21c32d2db79866c26a4eca
Edx <20975616+SimiCode@users.noreply.github.com>
2017-12-18 20:11:57 -0500

    changed_to soma's scrpit

--- sele_test.py
+++ sele_test.py
@@ -3,7 +3,10 @@ from selenium import webdriver
 
 with Display():
     # we can now start Firefox and it will run inside the virtual display
-    browser = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument('--no-sandbox')
+
+    driver = webdriver.Chrome(chrome_options=options)
 
     # put the rest of our selenium code in a try/finally
     # to make sure we always clean up at the end
