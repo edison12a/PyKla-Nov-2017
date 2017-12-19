b635dc020cc31df6e39bb409d035daf7ace5d07b
Edx <20975616+SimiCode@users.noreply.github.com>
2017-12-18 19:53:55 -0500

    Created a test file for DO

---
+++ sele_test.py
@@ -0,0 +1,15 @@
+from pyvirtualdisplay import Display
+from selenium import webdriver
+
+with Display():
+    # we can now start Firefox and it will run inside the virtual display
+    browser = webdriver.Firefox()
+
+    # put the rest of our selenium code in a try/finally
+    # to make sure we always clean up at the end
+    try:
+        browser.get('http://www.google.com')
+        print browser.title #this should print "Google"
+
+    finally:
+        browser.quit()
